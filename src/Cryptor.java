8264e775765572fa5bf32e74c88843b312d842b0
robotdorog <robotdorog@gmail.com>
2021-10-17 19:16:36 +0800

    Согласно замечаниям убрал два дублирущих метод. Оставил методы writer и reader. Убрал из класса Cryptor пути. Пути принимаются в параметрах метода

--- src/Cryptor.java
+++ src/Cryptor.java
@@ -4,15 +4,12 @@ import java.nio.file.Path;
 
 public class Cryptor {
 
-    public String crypt(String str) throws IOException {
-        // решить вопрос с обработкой пустой строки
+    public String crypt(String toCryptFilePath, String cryptFileResultPath) throws IOException {
 
         String result = "";
-        if (str == null) {
+        String strToCrypt = reader(toCryptFilePath);
 
-            return result;
-        } else {
-            String strToCrypt = CryptReader();
+        if (strToCrypt != null) {
             int count = 0;
             char currentChar = 0;
 
@@ -36,115 +33,77 @@ public class Cryptor {
                 result += count;
             } else
                 result += strToCrypt.charAt(strToCrypt.length() - 1);
-
-
-            if (Files.exists(Path.of("C:\\Users\\Usr1\\IdeaProjects\\Crypt\\src\\tasks\\file2.txt"))) {
-                CryptWriter(result);
-            } else Files.createFile(Path.of("C:\\Users\\Usr1\\IdeaProjects\\Crypt\\src\\tasks\\file2.txt"));
-            CryptWriter(result);
-
+            if (Files.exists(Path.of(cryptFileResultPath))) {
+                writer(result, cryptFileResultPath);
+            } else Files.createFile(Path.of(cryptFileResultPath));
+            writer(result, cryptFileResultPath);
+            return result;
+        } else {
+            return result;
         }
-
-        return result;
     }
 
-    public String decrypt() throws IOException {
+    public String decrypt(String toDecryptFilePath, String decryptFileResultPath) throws IOException {
 
         String result = "";
-        String strToDecrypt = DeCryptReader();
+        String strToDecrypt = reader(toDecryptFilePath);
 
-        char currentChar = 0;
-        int count = 0;
-        for (int i = 0; i < strToDecrypt.length(); i++) {
-            if (Character.isDigit(strToDecrypt.charAt(i))) {
-                count = Character.getNumericValue(strToDecrypt.charAt(i));
+        if (strToDecrypt != null) {
+            char currentChar = 0;
+            int count = 0;
+            for (int i = 0; i < strToDecrypt.length(); i++) {
+                if (Character.isDigit(strToDecrypt.charAt(i))) {
+                    count = Character.getNumericValue(strToDecrypt.charAt(i));
+                    for (int j = 1; j < count; j++) {
+                        result += currentChar;
+                    }
+                } else {
+                    result += currentChar;
+                }
+                if (!Character.isDigit(strToDecrypt.charAt(i)))
+                    currentChar = strToDecrypt.charAt(i);
+            }
+
+            if (!Character.isDigit(strToDecrypt.charAt(strToDecrypt.length() - 1)))
+                result += currentChar;
+            else {
+                for (int i = 0; i < strToDecrypt.length(); i++)
+                    count = Character.getNumericValue(strToDecrypt.charAt(i));
                 for (int j = 1; j < count; j++) {
 
                     result += currentChar;
                 }
-
-            } else {
-                result += currentChar;
             }
-            if (!Character.isDigit(strToDecrypt.charAt(i)))
-                currentChar = strToDecrypt.charAt(i);
-
+            if (Files.exists(Path.of(decryptFileResultPath))) {
+                writer(result, decryptFileResultPath);
+            } else Files.createFile(Path.of(decryptFileResultPath));
+            writer(result, decryptFileResultPath);
+            return result;
+        } else {
+            return result;
         }
-
-        if (!Character.isDigit(strToDecrypt.charAt(strToDecrypt.length() - 1)))
-            result += currentChar;
-        else {
-            for (int i = 0; i < strToDecrypt.length(); i++)
-                count = Character.getNumericValue(strToDecrypt.charAt(i));
-            for (int j = 1; j < count; j++) {
-
-                result += currentChar;
-            }
-
-        }
-
-        if (Files.exists(Path.of("C:\\Users\\Usr1\\IdeaProjects\\Crypt\\src\\tasks\\file4.txt"))) {
-            DeCryptWriter(result);
-        } else Files.createFile(Path.of("C:\\Users\\Usr1\\IdeaProjects\\Crypt\\src\\tasks\\file4.txt"));
-
-        DeCryptWriter(result);
-        return result;
     }
 
+    private void writer(String Result, String filePath) {
 
-    private void CryptWriter(String str) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Usr1\\IdeaProjects\\Crypt\\src\\tasks\\file2.txt"))) {
-            writer.write(str);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(Result);
             writer.flush();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-
     }
 
-    private void DeCryptWriter(String str) {
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Usr1\\IdeaProjects\\Crypt\\src\\tasks\\file4.txt"))) {
-            writer.write(str);
-            writer.flush();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-
-    }
-
-    private String CryptReader() {
+    private String reader(String filePath) {
         BufferedReader reader;
         String str = "";
         try {
-            reader = new BufferedReader(new FileReader("C:\\Users\\Usr1\\IdeaProjects\\Crypt\\src\\tasks\\file1.txt"));
-
+            reader = new BufferedReader(new FileReader(filePath));
             str = reader.readLine();
-
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return str;
-
-    }
-
-    private String DeCryptReader() {
-        BufferedReader reader;
-        String str = "";
-        try {
-            reader = new BufferedReader(new FileReader("C:\\Users\\Usr1\\IdeaProjects\\Crypt\\src\\tasks\\file3.txt"));
-
-            str = reader.readLine();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         return str;
     }
-
 }
 
--- src/main.java
+++ src/main.java
@@ -5,8 +5,8 @@ public class main {
 
     public static void main(String[] args) throws IOException {
         Cryptor cryptor = new Cryptor();
-        System.out.println(cryptor.crypt("C:\\Users\\Usr1\\IdeaProjects\\Crypt\\src\\tasks\\file1.txt"));
-        System.out.println(cryptor.decrypt());
+        System.out.println(cryptor.crypt("C:\\Users\\Usr1\\IdeaProjects\\Cryptor\\src\\tasks\\file1.txt", "C:\\Users\\Usr1\\IdeaProjects\\Cryptor\\src\\tasks\\file2.txt" ));
+        System.out.println(cryptor.decrypt("C:\\Users\\Usr1\\IdeaProjects\\Cryptor\\src\\tasks\\file3.txt", "C:\\Users\\Usr1\\IdeaProjects\\Cryptor\\src\\tasks\\file4.txt" ));
 
     }
 }
